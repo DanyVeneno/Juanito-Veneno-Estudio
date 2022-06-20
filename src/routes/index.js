2662e5a2aa35970501a027879f12dfe7599e13c2
Daniel Vallejo <yehiibhiidigital@gmail.com>
2022-06-19 20:51:24 -0500

    set service

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,7 @@ const router = Router()
 
 router.get('/',(req, res) => res.render('index', { title:'Juanito Veneno Estudio | Inicio' }))
 router.get('/about',(req, res) => res.render('about', { title:'Juanito Veneno Estudio | Nosotros' }))
+router.get('/servicios',(req, res) => res.render('servicios', { title:'Juanito Veneno Estudio | Servicios' }))
 router.get('/contact',(req, res) => res.render('contact', { title:'Juanito Veneno Estudio | Contacto' }))
 
 
